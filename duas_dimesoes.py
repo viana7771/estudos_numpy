739780df875fe945538710956939aac6ce12f79c
Kaire Viana <kaireviana20@gmail.com>
2026-09-02 16:15:51 -0300

    melhorando a matriz com funções

--- duas_dimesoes.py
+++ duas_dimesoes.py
@@ -1,14 +1,26 @@
 import numpy as np
 
-def main():
+def criar_matiz():
     matriz = ([
         [1, 2, 3],
         [4, 5, 6,],
         [7, 8, 9,]
     ])
-    
-    for mt in matriz:
-        print(mt)
+    array = np.array(matriz)
+
+    return array
+
+def mostrando_primeiro_elemento(array):
+    return array[2, 1]
+
+def main():
+    matriz = criar_matiz()
+
+    primeiro_elemento = mostrando_primeiro_elemento(matriz)
+
+    print(matriz)
+    print(f'\nPrimeiro elemento: {primeiro_elemento}')
+
 
 if __name__ == '__main__':
     main()
