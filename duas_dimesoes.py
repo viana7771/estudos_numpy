18ab7261d9474f4240b86ab9da85361447738a82
Kaire Viana <kaireviana20@gmail.com>
2026-09-02 14:32:59 -0300

    matriz de 3 dimenssões

---
+++ duas_dimesoes.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def main():
+    matriz = ([
+        [1, 2, 3],
+        [4, 5, 6,],
+        [7, 8, 9,]
+    ])
+    
+    for mt in matriz:
+        print(mt)
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def main():  # Criando meu primeiro array
+    lista = [
+        10, 20, 30, 40, 50
+    ]
+    array = np.array(lista)
+    print(array)
+
+if __name__ == '__main__':
+    main()
